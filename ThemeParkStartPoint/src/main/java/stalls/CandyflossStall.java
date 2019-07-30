057bd777009bf756ddb3a0149792f16f1dde94d1
Chae Nicholson <chae.nicholson@gmail.com>
2019-07-30 22:41:03 +0100

    Implemented IReviewed for Candyflossstalll, added getRating method and tested

--- ThemeParkStartPoint/src/main/java/stalls/CandyflossStall.java
+++ ThemeParkStartPoint/src/main/java/stalls/CandyflossStall.java
@@ -1,8 +1,16 @@
 package stalls;
 
-public class CandyflossStall extends Stall {
+import behaviours.IReviewed;
 
-    public CandyflossStall(String name, String ownerName, ParkingSpot parkingSpot) {
+public class CandyflossStall extends Stall implements IReviewed {
+    int rating;
+
+    public CandyflossStall(String name, String ownerName, ParkingSpot parkingSpot, int rating) {
         super(name, ownerName, parkingSpot);
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return rating;
     }
 }
