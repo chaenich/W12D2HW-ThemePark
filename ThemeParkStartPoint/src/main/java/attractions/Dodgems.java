16926a7ba4052156d4e26eb7474a1bc3e90ca7d7
Chae Nicholson <chae.nicholson@gmail.com>
2019-07-30 20:25:08 +0100

    Simplified dodgems default price logic and now merely return hardcoded starting price, rather than passing in starting price as param to new dodgem

--- ThemeParkStartPoint/src/main/java/attractions/Dodgems.java
+++ ThemeParkStartPoint/src/main/java/attractions/Dodgems.java
@@ -6,14 +6,12 @@ import people.Visitor;
 public class Dodgems extends Attraction implements ITicketed {
     private double defaultPrice;
 
-    public Dodgems(String name, int rating, double defaultPrice) {
+    public Dodgems(String name, int rating) {
         super(name, rating);
-        this.defaultPrice = defaultPrice;
     }
 
     public double defaultPrice() {
-        return this.defaultPrice;
-
+        return 4.50;
     }
 
 }
